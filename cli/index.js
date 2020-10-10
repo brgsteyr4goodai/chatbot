73fd510ad9c2d2ded402efb0d4b591e16ee00220
OliverKovacs <oliver.kovacs.dev@gmail.com>
2020-10-10 20:12:55 +0200

    Added colors to cli

--- cli/index.js
+++ cli/index.js
@@ -4,14 +4,26 @@ const readline = require("readline");
 const Bot = require("../bot/index.js");
 const bot = new Bot();
 
+const colors = require(`${__dirname}/colors.json`);
+const config = require(`${__dirname}/config.json`);
+let { style } = config;
+
+for (const color in colors) {
+    colors[color] = `\x1b[${colors[color]}m`;
+}
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 const loop = answer => {
-    console.log(`Bot: ${bot.message(answer)}`);
-    rl.question("You: ", loop);
+    console.log(`${format("Bot", colors[style.botname])}: ${format(bot.message(answer), colors[style.bottext])}`);
+    rl.question(`${format("You", colors[style.usrname])}: ${colors[style.usrtext]}`, loop);
+};
+
+const format = (string, ...color) => {
+    return `${colors.reset}${color.join("")}${string}${colors.reset}`;
 };
 
 loop("Hello");
