7abef40be2ef50dc49b640f47e86ea2f9678b155
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2020-10-16 22:04:00 +0200

    Basic dialogflow test implementation

--- bot/index.js
+++ bot/index.js
@@ -1,15 +1,44 @@
 const config = require(`${__dirname}/config.json`);
 const DialogFlow = require("@google-cloud/dialogflow");
-
+const uuid = require('uuid');
 const Symptoma = require("./api/symptoma.js");
 
 class Bot {
     constructor(...args) {
+        this.client = new DialogFlow.SessionsClient({
+            keyFilename : `${__dirname}/credentials/public.json`
+        });
+        this.sessionId = uuid.v4();
 
+        this.path = this.client.projectAgentSessionPath(config.projectID, this.sessionId)
     }
 
     async message(msg, ts = Date.now()) {
-        return msg;                         // for testing
+        if (typeof msg !== "string") {
+            throw "Invalid type passed to message";
+        }
+
+        let query = {
+            session : this.path,
+            queryInput : {
+                text : {
+                    text : msg,
+                    languageCode: config.languageCode
+                }
+            }
+        }
+
+        let response = await this.client.detectIntent(query);
+        response = response[0].queryResult;
+
+        let reply = response.fulfillmentText;
+        try {
+            if (response.parameters.fields.Symptoms.listValue.values.length > 0) {
+                reply += `  >> Symptom Params: ${JSON.stringify(response.parameters.fields.Symptoms.listValue.values.map(v => v.stringValue))}`
+            }
+        } catch (e) {}
+
+        return reply;
     }
 }
 
