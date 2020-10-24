9f422d1e60b21bd20d41f1c1654ddc06ddd0f3b0
OliverKovacs <oliver.kovacs.dev@gmail.com>
2020-10-24 13:06:54 +0200

    Added Wikipedia API

---
+++ bot/api/wikipedia.js
@@ -0,0 +1,89 @@
+
+const https = require("https");
+const querystring = require("querystring");
+
+class Wikipedia {
+    
+    static options = {
+        hostname: "en.wikipedia.org",
+        port: 443,
+        path: "/",
+        method: "GET",
+        headers : {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    static queryobj = {
+        "action": "query",
+        "format": "json"
+    };
+
+    static query(obj) {
+        return new Promise((resolve, reject) => {
+            const qstring = querystring.stringify({ ...this.queryobj, ...obj });
+            const path = `/w/api.php?${qstring}`;
+            const options = { ...this.options, path };
+
+            let req = https.request(options, res => {
+                let data = "";
+
+                res.on("data", chunck => {
+                    data += chunck;
+                });
+
+                res.on("end", () => {
+                    data = JSON.parse(data);
+                    resolve(data);
+                });
+            });
+            
+            req.on("error", error => {
+                console.error(error);
+                reject(error);
+            });
+            
+            req.end();
+        });
+    }
+
+    static async article(id) {
+        return await this.query({
+            "prop": "info",
+            "pageids": id,
+            "inprop": "url",
+        });
+    }
+
+    static async extract(id) {
+        return await this.query({
+            "prop": "extracts",
+            "explaintext": "1",
+            "pageids": id,
+        });
+    }
+
+    static async search(term) {
+        return await this.query({
+            "list": "search",
+            "srsearch": term,
+        });
+    }
+
+    static getId(res) {
+        return res.query.search[0].pageid;
+    }
+
+    static getPage(res) {
+        let pages = res.query.pages;
+        for (let id in pages) {
+            return pages[id];
+        }
+    }
+
+    static getFirstParagraph(extract) {
+        return extract.extract.split("\n\n\n")[0];
+    }
+}
+
+module.exports = Wikipedia;
