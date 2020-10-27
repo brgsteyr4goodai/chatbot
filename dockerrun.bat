bc120f2ea82bcf4ad22005d28c7853414e11c8b1
OliverKovacs <oliver.kovacs.dev@gmail.com>
2020-10-27 19:32:15 +0100

    Added dockerrun/ignore

---
+++ dockerrun.bat
@@ -0,0 +1,5 @@
+docker build -t chatbot -f .docker/Dockerfile .
+docker run -dt --name cb chatbot
+docker exec -it cb sh -c "cd /workspaces/chatbot && npm start"
+docker stop -t 0 cb
+docker container rm cb
