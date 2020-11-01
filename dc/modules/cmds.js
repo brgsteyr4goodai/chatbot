0ca4b51880f92250b98bd0e044a2424f857ae76d
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2020-11-01 20:07:06 +0100

    Discord QOL changes

--- dc/modules/cmds.js
+++ dc/modules/cmds.js
@@ -1,11 +1,12 @@
 let botClass = require("../../bot");
 const utils = require("./utils.js");
-const { MessageEmbed : Embed } = require("discord.js");
+const Embed = require("ccord").Discord.MessageEmbed;
 const config = require("../config.json");
 const { contributors } = require("../../package.json");
+const { spawnSync } = require("child_process")
 
 module.exports = class {
-    bind (msg, active) {
+    listen (msg, active) {
         active[utils.chId(msg)] = {
             origin : msg,
             author : msg.author,
@@ -14,13 +15,13 @@ module.exports = class {
         }
 
         let embed = new Embed()
-            .setTitle("Now listening to messages in this channel ("+msg.channel.name+")")
+            .setTitle("Now listening to messages in this channel (#"+msg.channel.name+")")
             .setFooter(msg.author.tag, msg.author.displayAvatarURL())
 
         msg.channel.send(embed);
     }
 
-    unbind (msg, active) {
+    silence (msg, active) {
         if (utils.chId(msg) in active) {
             if (active[utils.chId(msg)].author.id === msg.author.id) {
                 delete active[utils.chId(msg)];
@@ -28,7 +29,7 @@ module.exports = class {
         }
 
         let embed = new Embed()
-            .setTitle("Stopped listening to messages in this channel ("+msg.channel.name+")")
+            .setTitle("Stopped listening to messages in this channel (#"+msg.channel.name+")")
             .setFooter(msg.author.tag, msg.author.displayAvatarURL())
 
         msg.channel.send(embed);
@@ -38,7 +39,7 @@ module.exports = class {
         if (cmd.length < 2) {
             let help = new Embed()
                 .setTitle("Help")
-                .setDescription(`Use ${config.prefix}help <command> to get info on that command, use ${config.prefix}setup to get basic info on how to use the bot`)
+                .setDescription(`Use \`${config.prefix}help <command>\` to get info on that command, use \`${config.prefix}setup\` to get basic info on how to use the bot`)
                 .addField("Commands", Object.getOwnPropertyNames(Object.getPrototypeOf(this)).filter(e => e !== "constructor").join("\n"));
 
             msg.channel.send(help);
@@ -58,7 +59,7 @@ module.exports = class {
 
     info (msg, ac, io, client) {
         let embed = new Embed()
-            .setTitle("Chatbot 4goodai")
+            .setTitle("Wilson Chatbot 4goodai")
             .setDescription("Developed for https://www.ada.wien/hackathon-fur-gute-ki-4goodai-2020/")
             .addField("Contributors", contributors.map(({ name }) => name).join("\n"))
             //.addField("Technologies used", ["[Discord.js](https://discord.js.org/#/)", "..."].join("\n"))
@@ -72,10 +73,30 @@ module.exports = class {
 
         let embed = new Embed()
             .setTitle("Usage")
-            .setDescription(`The commands ${prefix}bind and ${prefix}unbind allow you to redirect the messages you send to the bot.
-            After using ${prefix}bind, all message from issuing user (in this channel) will be redirected to the bot.
-            ${prefix}unbind removes this message forwarding. 
+            .setDescription(`The commands \`${prefix}listen\` and \`${prefix}silence\` allow you to redirect the messages you send to the bot.
+            After using \`${prefix}listen\`, all message from issuing user (in this channel) will be redirected to the bot.
+            \`${prefix}silence\` removes this message forwarding. 
             `)
+
+        msg.channel.send(embed)
+    }
+
+    stats (msg, active) {
+        let git
+
+        try {
+            git = spawnSync("git", ["rev-list", "--all", "--count"] , { encoding : 'utf8' }).stdout;
+        } catch (e) {
+            msg.channel.send("Error: Host does not support stats command.");
+            return;
+        }
+
+
+        let embed = new Embed()
+            .setTitle("Discord statistics")
+            .addField("Current bindings", Object.keys(active).length)
+            .addField("Available commands", Object.getOwnPropertyNames(Object.getPrototypeOf(this)).filter(e => e !== "constructor").length)
+            .addField("Version", git ? "v" + git : "Not available")
 
         msg.channel.send(embed)
     }
--- dc/modules/help.js
+++ dc/modules/help.js
@@ -1,7 +1,8 @@
 module.exports = {
-    bind : "Pipe all input from this channel and user to the chatbot. See setup command for more info.",
-    unbind : "Stop listening for user input from this user in this channel. See setup command for more info.",
+    listen : "Pipe all input from this channel and user to the bot. See setup command for more info.",
+    silence : "Stop listening for user input from this user in this channel. See setup command for more info.",
     help : "Help command.",
     info : "Info about the bot.",
-    setup : "Command that shows info on the functionality of the bot."
+    setup : "Command that shows info on the functionality of the bot.",
+    stats : "Show some statistics on the discord bot"
 }
